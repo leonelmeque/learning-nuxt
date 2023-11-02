4ea9a639cbf720b9f400136e61aba573cad5d327
Leonel Meque <leonel.meque@glovoapp.com>
2023-11-02 13:09:45 +0100

    dynamic route params

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/movies">Movies</NuxtLink>
+      <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+    </nav>
+  <NuxtPage />
+  </div>
+</template>
---
+++ pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,8 @@
+<script setup>
+const route = useRoute()
+const message = computed(()=>`I am on a team ${route.params.teamSlug} and my user id is ${route.params.userId}`)
+</script>
+
+<template>
+  {{message}}
+</template>
