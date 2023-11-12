539add70e78d6577d4e1e37e86e6083df3b66007
Leonel Meque <leomeque@gmail.com>
2023-11-12 01:14:40 +0100

    middleware

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<script setup>
+  definePageMeta({
+    layout: 'plain',
+  })
+</script>
+
+<template>
+  <form action="">
+    <h1>Login</h1>
+    <label for=""
+      >Username
+      <input type="text" />
+    </label>
+    <label for="">
+      Password
+      <input type="text" />
+    </label>
+  </form>
+</template>
--- pages/movies/index.vue
+++ pages/movies/index.vue
@@ -1,7 +1,7 @@
+<script setup>
+  import MovieSearch from '~/components/MovieSearch.vue'
+</script>
+
 <template>
   <MovieSearch />
 </template>
-
-<script lang="ts" setup>
-  import MovieSearch from '~/components/MovieSearch.vue'
-</script>
